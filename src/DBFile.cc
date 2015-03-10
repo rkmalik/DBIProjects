08b9164b2dae869794c53123b5ea82eff376a21f
rkmalik <rkmalik@ufl.edu>
2015-03-09 20:38:01 -0400

    Updated File for Project 2 Milestone 2

--- src/DBFile.cc
+++ src/DBFile.cc
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "TwoWayList.h"
 #include "Record.h"
 #include "Schema.h"
@@ -7,95 +6,186 @@
 #include "ComparisonEngine.h"
 #include "DBFile.h"
 #include "Defs.h"
-
-using namespace std;
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
 // stub file .. replace it with your own DBFile.cc
 
+using namespace std;
+/*
+ *Default Constructor.
+ */
 DBFile::DBFile () {
-	mode = heap;
-	mydbfile = NULL;
+    g_return_value=1;
 }
 
-DBFile::~DBFile () {
-	delete mydbfile;
+/*
+ *This method is used to create an instance of a heap file.
+ */
+int DBFile::Create (char *file_path, fType file_type, void *startup) {
+    if(file_path == NULL)
+        return 0;
+    if(file_type != heap && file_type != sorted)
+        return 0;
+    string s = (string) file_path;
+    if(s.substr(s.find_last_of(".") + 1) != "bin" && s.substr(s.find_last_of(".") + 1) != "bigq"){
+        return 0;
+    }
+    if(startup == NULL && file_type == sorted)
+        return 0;
+    //Creating the meta file
+    string meta_file_Name;
+    meta_file_Name.append(file_path);
+    meta_file_Name.append(".meta");
+    ofstream meta_file;
+    meta_file.open(meta_file_Name.c_str());
+    meta_file << file_type << endl; // write db type to the meta file
+    //Writing contents of the db type to meta file
+    if(sorted == file_type)
+      {
+        SortInfo si = *((SortInfo *)startup);
+        meta_file << si.runLength << endl;
+        OrderMaker om = *(OrderMaker *)si.myOrder;
+        meta_file << om.numAtts << endl;
+        for (int i = 0; i < om.numAtts; i++) {
+          meta_file << om.whichAtts[i] << " ";
+          meta_file << om.whichTypes[i] << endl;
+        }
+      }
+    meta_file.close();
+
+    //End of the meta file block
+    switch(file_type)
+    {
+    case heap:// If the file is of type heap
+      cout << "Opening in heap file mode." <<  endl;
+      genericFile = new HeapDBFile();
+      break;
+    case sorted:
+      cout << "create a sorted dbfile" << endl;
+      genericFile = new SortedDBFile();
+      break;
+    default:
+      cout << "File Type not identified." <<  endl;
+      exit(-1);
+    }
+  //Call to the create method of the generic version of the file
+  return genericFile->Create(file_path, file_type, startup);
 }
 
-int DBFile::Create (char *f_path, fType f_type, void *startup) {
-
-	switch (heap)
-	{
-		case heap:
-
-			mydbfile = new HeapDBFile();
-			if (mydbfile== NULL){
-				cout << "Failed to Create a Heap File" << endl;
-			}
-			cout<< "Heap File Created" << endl;
-			return mydbfile->Create (f_path, startup);
-
-			break;
-
-		case sorted:
-			break;
-
-		case tree:
-			break;
-
-		default:
-			break;
-	}
-
-
-	return 0;
+int DBFile::Open (char *file_path) {
+    if(file_path == NULL)
+        return 0;
+    string s = (string) file_path;
+    if(s.substr(s.find_last_of(".") + 1) != "bin"){
+        return 0;
+    }
+    //Reading File Type from meta file
+    int type;
+    string meta_file_Name;
+    meta_file_Name.append(file_path);
+    meta_file_Name.append(".meta");
+    ifstream meta_file;
+    meta_file.open(meta_file_Name.c_str());
+    meta_file >> type;
+    fType db_file_Type = (fType) type;
+    meta_file.close();
+    //Closing the meta file
+    switch(db_file_Type){
+        case heap:
+            cout << "Opening a heaped dbfile" << endl;
+            genericFile = new HeapDBFile();
+            break;
+        case sorted: // fall through, not implemented
+            cout << "Opening a sorted dbfile" << endl;
+            genericFile = new SortedDBFile();
+            break;
+        default:
+            cout << "Unidentified File Type" <<  endl;
+            exit(-1);
+    }
+    return genericFile->Open(file_path);
 }
+
 
 void DBFile::Load (Schema &f_schema, char *loadpath) {
-	mydbfile->Load (f_schema, loadpath);
-
+    //Check for NULL load path
+    if(loadpath == NULL){
+        cout << "Load Path is NULL"<<endl;
+        g_return_value = 0;
+        return;
+    }
+    //Check for Schema value to be NULL
+    if(!&f_schema){
+        g_return_value = 0;
+        return;
+    }
+    //check for valid load path
+    FILE *tableFile = fopen (loadpath, "r");
+    if (0 == tableFile){
+        g_return_value = 0;
+        return;
+    }
+    if(genericFile == NULL){
+        g_return_value = 0;
+        return;
+    }
+    string s = (string) loadpath;
+    if(s.substr(s.find_last_of(".") + 1) != "tbl"){
+        g_return_value = 0;
+        return;
+    }
+    genericFile->Load(f_schema,loadpath);
 }
 
-int DBFile::Open (char *f_path) {
-	int ret = 0;
-	cout << "Opening the file " << f_path<< endl;
-	if (mydbfile==NULL)
-		mydbfile = new HeapDBFile();
 
-	ret = mydbfile->Open (f_path);
-	if (ret == 1)
-		cout << "Heap File is Opened" << endl;
-
-	return ret;
-
-}
-
+/*
+ * This function forces the pointer to correspond to the first record in the file.
+ */
 void DBFile::MoveFirst () {
-	mydbfile->MoveFirst ();
+    if(genericFile == NULL){
+        g_return_value = -1;
+        return;
+    }
+    genericFile->MoveFirst();
 }
 
 int DBFile::Close () {
-	// Calling the Base class Close method to close the file
-	mydbfile->Close();
+    int output = genericFile->Close();
+    return output;
 }
 
 void DBFile::Add (Record &rec) {
-    // This method is implemented in HeapFile which is called from the mydbfile Pointer.
-    mydbfile->Add(rec);
+    //Check for null record
+    if(!&rec){
+        g_return_value = -1;
+        return;
+    }
+    genericFile->Add(rec);
 }
 
-void DBFile::AddPage() {
-    mydbfile->AddPage();
+int isValidRecord(Record &record)
+{
+    if(!&record)
+        return 0;
+    return 1;
 }
+
 int DBFile::GetNext (Record &fetchme) {
-	mydbfile->GetNext (fetchme);
+    if(isValidRecord(fetchme)==0)
+        return 0;
+    return genericFile->GetNext(fetchme);
 }
-
-
-
-void DBFile::GetPage (Page *putItHere, off_t whichPage)  {
-	mydbfile->GetPage (putItHere, whichPage);
-}
-
 
 int DBFile::GetNext (Record &fetchme, CNF &cnf, Record &literal) {
-    return mydbfile->GetNext(fetchme, cnf, literal);
+    if(isValidRecord(fetchme)==0)
+        return 0;
+    if(!&cnf)
+        return 0;
+
+    if(!&literal)
+        return 0;
+
+    return genericFile->GetNext(fetchme, cnf, literal);
 }
--- src/DBFile.h
+++ src/DBFile.h
@@ -4,45 +4,38 @@
 #include "TwoWayList.h"
 #include "Record.h"
 #include "Schema.h"
+#include "BaseFile.h"
 #include "File.h"
+#include "HeapDBFile.h"
+#include "SortedDBFile.h"
 #include "Comparison.h"
 #include "ComparisonEngine.h"
-#include "HeapDBFile.h"
-#include "BaseFile.h"
-//class BaseFile;
 
-typedef enum {heap, sorted, tree} fType;
 
 // stub DBFile header..replace it with your own DBFile.h
 
 class DBFile {
-
-	fType mode;
-	BaseFile * mydbfile;
-
-
+private:
+  BaseFile * genericFile;
 public:
-
 	DBFile ();
-	~DBFile ();
-
+    int g_return_value;
 	int Create (char *fpath, fType file_type, void *startup);
 	int Open (char *fpath);
 	int Close ();
 
 	void Load (Schema &myschema, char *loadpath);
 
+
 	void MoveFirst ();
 	void Add (Record &addme);
-    void AddPage();
 	int GetNext (Record &fetchme);
-	// int GetNext (Record &fetchme, CNF &cnf, Record &, Schema * p=NULL);
 	int GetNext (Record &fetchme, CNF &cnf, Record &literal);
-	void GetPage (Page *putItHere, off_t whichPage);
-    off_t GetLength () {
-        return mydbfile->GetLength();
-    }
-
-
+	void setBaseFileNull(){
+        this->genericFile = NULL;
+	}
 };
+
+
 #endif
+
