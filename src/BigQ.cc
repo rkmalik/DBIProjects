93200fec914b931730997e4358e8077d23eae259
rkmalik <rkmalik@ufl.edu>
2015-03-09 20:30:11 -0400

    Updated File for Project 2 Milestone 2

--- src/BigQ.cc
+++ src/BigQ.cc
@@ -1,211 +1,189 @@
 #include "BigQ.h"
-#include <pthread.h>
-#include "ComparisonEngine.h"
-#include <vector>
-#include <algorithm>
-#include "Record.h"
-#include "DBFile.h"
-#include "HeapDBFile.h"
-
-void *tpmms(void* arg) {
-    BigQutil *bigQutil = (BigQutil *) arg;
-    Record * temp = new Record();
-    DBFile tempfile;
-    tempfile.Create ("./bin/temp", heap, NULL);
-    tempfile.Open("./bin/temp");
 
 
-    int runlength = bigQutil->runlen;
-    int run_size= PAGE_SIZE * runlength;
+using namespace std;
 
-    // This holds the Page number of each run head
-    vector <int> runhead;
-    int pageNum = 0;
-    int totalsize =0;
-    vector<Record *> recArray;
-    runhead.push_back(pageNum);
-    int page_count =0;
-    int tempcounter = 0;
+void BigQ::mergeRuns()
+{
+	cout<<"Merging the Runs"<<endl;
+	tempFile.Open(1,tempFileName);
+   	priority_queue<RunRecord *, vector<RunRecord *>, CompareQueues> pQueue(&orderMaker);
 
-    while(bigQutil->in->Remove(temp)) {
-        //bigQutil->out->Insert(&temp);
-        totalsize += temp->GetSize();
+	Page *inputPage  = new Page[runCounter];
 
-       // cout << totalsize << "   " <<  temp->GetSize () << endl;
-        if(totalsize > PAGE_SIZE) {
-            page_count++;
-            pageNum++;
-            totalsize = temp->GetSize();
+	int whichPage[runCounter];
+
+	vector<int> runStart;
+	for(int i=0; i<runCounter; i++)
+	{
+        int count=0;
+        for(vector<int>::iterator it=runL.begin(); it!=runL.begin()+i; it++)
+        {
+            count+=*it;
         }
-
-        if(page_count == runlength) {
-            sort(recArray.begin(),recArray.end(),RecordComparator(bigQutil->order));
-            //cout << "Run " << tempcounter++ << " has " << recArray.size () << " Records." << endl;
-            for (std::vector<Record *>::iterator it=recArray.begin(); it!=recArray.end(); ++it) {
-                tempfile.Add(*(*it));
-                //bigQutil->out->Insert((*it));
-            }
-            recArray.clear();
-            recArray.push_back(temp);
-            runhead.push_back(pageNum);
-            temp = new Record();
-            page_count =0;
-        }
-        else {
-            recArray.push_back(temp);
-            temp = new Record();
-        }
-    }
-    //no more records in input pipe
-    sort(recArray.begin(),recArray.end(),RecordComparator(bigQutil->order));
-   // cout << "Run " << tempcounter++ << " has " << recArray.size () << " Records." << endl;
-    for (std::vector<Record *>::iterator it=recArray.begin(); it!=recArray.end(); ++it) {
-        tempfile.Add(*(*it));
-        //bigQutil->out->Insert((*it));
-    }
-    recArray.clear();
-    pageNum++;
-    runhead.push_back(pageNum);
-    tempfile.AddPage(); // last page is added explicitly
-
-
-    // Imlementing the external sorting mechanism.
-
-    /*
-        Read the data from the file and put the records in the Priority Queue
-        Based on the comparison criteria. Get the maximum item from priority qu5/eue.
-        and put that in the external pipe. At the same time put the data in teh saame run
-        from which data is just removed.
-
-        Run head is holding the top page number of each head.
-    */
-  //  cout << "filsize  "<<tempfile.GetLength() << "--"<<runhead.size()<<endl;
-    priority_queue <pair<int, Record*>, vector <pair <int, Record*> >, PairSorter> sortq (PairSorter (*(bigQutil->order)));
-    vector <int> runcheck (runhead);
-    vector <Page *> runpagelist;
-
-
-    int arr [runhead.size ()];
-
-    //
-    // Now I will initialize the data in this priority queue.
-    for (int i = 0; i < runhead.size ()-1; i++)
-    {
-       // cout << "Reached Here 1" << "  Run Head " << runhead[i]+1 << endl;
-       arr[i] = 0;
-        Page * mypage = new Page ();
-        tempfile.GetPage(mypage, runhead[i]);
-       //     cout << "Reached Here 2" << endl;
-        // Load the page from the file
-        Record * myrec = new Record ();
-        mypage->GetFirst(myrec);
-        ++arr [i];
-        pair<int, Record *> * mypair = new pair<int, Record*>  (i, myrec);
-        sortq.push (*mypair);
-        runpagelist.push_back (mypage);
-
-        //cout << "Reached Here 1" << endl;
-    }
-
-    int size = (sizeof (arr)/ sizeof (int));
-    /**for (int i = 0; i < size; i++)
-        cout << arr [i] << " ";
-
-        cout << endl << endl;**/
-
-    // Till now I have taken records from each run and put it into the priority queue.
-    // Now i need to pick the first record from the top and put that into the output pipe.
-
-    while (!sortq.empty ())
-    {
-        pair<int, Record*>  mypairtemp = (sortq.top ());
-        int runnum = mypairtemp.first;
-        Record * myrec = mypairtemp.second;
-        bigQutil->out->Insert(myrec);
-        sortq.pop ();
-
-        int ret = runpagelist[runnum]->GetFirst(myrec);
-
-        //if (ret )
-
-
-        // Now take the record from the RunNumber +1; and place that in the priority queue
-        if(!ret) {
-            // Check if the current Offset is less than the next run start offset then it is fine to take the new record
-            if (++runcheck[runnum]< runhead[runnum+1]) {
-
-                Record * myrec = new Record ();
-                Page * mypage = new Page ();
-                tempfile.GetPage(mypage, runcheck[runnum]);
-                mypage->GetFirst(myrec);
-                pair<int, Record *> * mypair = new pair<int, Record*>  (runnum, myrec);
-                ++arr [runnum];
-                sortq.push (*mypair);
-                runpagelist[runnum] = mypage;
-            } else {
-
-                //cout << "I have sucked all the records from " << runnum << endl;
-
-            }
-
-        } else {
-            /*Record * myrec = new Record ();
-            Page * mypage = new Page ();
-            tempfile.GetPage(mypage, runhead[i]);
-            mypage.GetFirst(myrec);*/
-             ++arr [runnum];
-            pair<int, Record *> * mypair = new pair<int, Record*>  (runnum, myrec);
-            sortq.push (*mypair);
-        }
-
-    }
-
-
-    /**for (int i = 0; i < size; i++)
-        cout << arr [i] << " ";
-
-        cout << endl << endl;**/
-
-    for(int i=0; i<runpagelist.size(); i++){
-		delete runpagelist[i];
+        runStart.push_back(count);
 	}
 
 
-    //free(bigQutil);
-    tempfile.Close();
+	//Initializing the first page of each run
+	for(int i=0;i<runCounter;++i)
+	{
+        tempFile.GetPage(&(inputPage[i]),runStart[i]);
+		whichPage[i]=0;
+	}
+	//Take the first record from eache page and initialize the queue.
+	for(int i=0; i<runCounter; i++)
+	{
+	    RunRecord *runRecord=new RunRecord;
+		runRecord->runNum=i;
+		inputPage[i].GetFirst(&(runRecord->record));
+		pQueue.push(runRecord);
+	}
+
+	while(true)
+	{
+		if(pQueue.empty())
+			break;
+		// Push the first record of the priority queue into the output pipe
+		RunRecord *outputRecord = pQueue.top();
+		pQueue.pop();
+		int runNum = outputRecord->runNum;
+		out->Insert(&(outputRecord->record));
+
+		// Push the next record from the runs into the priority queue
+		RunRecord *nextRunRecord=new RunRecord;
+
+		// If the run page gets over...
+		if(inputPage[runNum].GetFirst(&(nextRunRecord->record)) == 0)
+		{
+			whichPage[runNum]++;
+			if(whichPage[runNum]>=runL[runNum])
+				continue;
+			tempFile.GetPage(&(inputPage[runNum]),whichPage[runNum]+runStart[runNum]);
+			inputPage[runNum].GetFirst(&(nextRunRecord->record));
+		}
+		nextRunRecord->runNum=runNum;
+		pQueue.push(nextRunRecord);
+	}
+
+
+    //finally shut down the out pipe
+    tempFile.Close();
+    delete [] inputPage;
 }
 
+void BigQ :: sortAndWriteToFile()
+{
+	if(myVector.empty())
+		return;
+	stable_sort(myVector.begin(),myVector.end(),CompareRecords(&orderMaker));
+	int runLen=0;
+	for(vector<Record *>::iterator myRecord=myVector.begin(); myRecord!=myVector.end(); myRecord++)
+	{
+		if(sortPage.Append(*myRecord))
+			continue;
+
+		writeToFile();
+		sortPage.Append(*myRecord);
+		runLen++;
+	}
+
+	writeToFile();
+	runLen++;
+	runCounter++;
+	runL.push_back(runLen);
+}
+
+void BigQ :: writeToFile()
+{
+	off_t fileLength=tempFile.GetLength();
+	off_t whichPage = (fileLength>0)?fileLength-1:0;
+
+	tempFile.AddPage(&sortPage, whichPage);
+    sortPage.EmptyItOut();
+}
+
+void BigQ :: freeResourcesPhaseOne()
+{
+	if(myVector.empty())
+	{
+		return;
+	}
+	for(vector<Record *>::iterator myRecord=myVector.begin(); myRecord!=myVector.end(); myRecord++)
+    {
+        delete *myRecord;
+    }
+    myVector.clear();
+	tempFile.Close();
+}
+
+void BigQ::generateRuns()
+{
+	cout<<"Going into generating runs phase"<<endl;
+	tempFile.Open(0,tempFileName);
+	while(in->Remove(&myRecord))
+	{
+	    Record *cRecord=new Record;
+	    cRecord->Copy(&myRecord);
+
+	    if(myPage.Append(&myRecord))
+	    {
+	        myVector.push_back(cRecord);
+	    }
+	    else
+	    {
+	        pageCounter++;
+	    	if(pageCounter==runLength)
+	    	{
+	        	sortAndWriteToFile();
+				myVector.clear();
+            	pageCounter=0;
+	    	}
+
+	        myPage.EmptyItOut();
+            myPage.Append(&myRecord);
+            myVector.push_back(cRecord);
+	    }
+	}
+
+	myPage.EmptyItOut();
+	sortPage.EmptyItOut();
+	sortAndWriteToFile();
+	cout<<"Number of runs : "<<runCounter<<endl;
+	//free vector
+    freeResourcesPhaseOne();
+}
+
+void* sortThread(void* arg)
+{
+    cout<<"Initializing the Run...."<<endl;
+    BigQ *bigQ = new BigQ();
+    bigQ = (BigQ*)arg;
+
+    int num=rand()%10000;
+	sprintf(bigQ->tempFileName,"%d.txt",num);
+
+	bigQ->generateRuns();
+	bigQ->mergeRuns();
+
+    cout<<"Completed Merging of the Runs.."<<endl;
+	bigQ->out->ShutDown ();
+}
 
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
-	// read data from in pipe sort them into runlen pages
-
-    // construct priority queue over sorted runs and dump sorted data
- 	// into the out pipe
-
-    // finally shut down the out pipe
-	// Initialize Data Structures do I need it ?
-	// Spawn a Worker thread
-	/**
-		Phase 1
-		read run length no. of pages from InputPipe in Memory and append to DBFile
-		Maintain run pointers for each run
-
-		Phase 2
-		Load r(no. of runs) buffers and have one output buffer
-		extract min. among r buffers and write it to output buffer
-		if the input buffer is empty load more from that run
-		if the output buffer is full write it to disk
-		if the run is completed scanning then mark it as inactive
-		repeat the steps until all runs are scanned completely
-	**/
-
-
-	BigQutil bigQutil = {&in,&out, &sortorder, runlen};
-	pthread_t worker_thread;
-    pthread_create(&worker_thread,NULL,tpmms,(void *)&bigQutil);
-    pthread_join(worker_thread,NULL);
-	out.ShutDown ();
+	this->in=&in;
+	this->out=&out;
+	this->orderMaker=sortorder;
+	this->runLength=runlen;
+    this->pageCounter = 0;
+    this->runCounter = 0;
+	pthread_t sortthread;
+  	pthread_create(&sortthread,NULL,&sortThread,(void*)this);
 }
 
 BigQ::~BigQ () {
+}
+
+BigQ :: BigQ() {
+
 }
--- src/BigQ.h
+++ src/BigQ.h
@@ -5,73 +5,77 @@
 #include "Pipe.h"
 #include "File.h"
 #include "Record.h"
+#include "Schema.h"
 #include "ComparisonEngine.h"
+#include <vector>
+#include <algorithm>
 #include <queue>
-
+#include <time.h>
 
 using namespace std;
 
-class RecordSorter {
-
-    OrderMaker & _sortorder;
-
-public:
-    RecordSorter (OrderMaker & sortorder):_sortorder(sortorder) { }
-
-    bool operator () (Record * r1, Record * r2) {
-        ComparisonEngine compengine;
-
-        if (compengine.Compare(r1, r2, &_sortorder) < 0)
-            return true;
-        else
-            return false;
-    }
-};
-
-class PairSorter {
-
-    OrderMaker & _sortorder;
-
-public :
-    PairSorter(OrderMaker & sortorder):_sortorder(sortorder){}
-
-    bool operator () (pair <int, Record *> r1, pair <int, Record *> r2)
-    {
-        ComparisonEngine compengine;
-
-        if (compengine.Compare(r1.second, r2.second, &_sortorder) < 0)
-            return false;
-        else
-            return true;
-
-    }
-};
-
-typedef struct {
-        Pipe *in;
-        Pipe *out;
-	    OrderMaker *order;
-        int runlen;
-} BigQutil;
-
-struct  RecordComparator {
-   OrderMaker *order;
-   RecordComparator(OrderMaker* order) {
-        this->order = order;
-   }
-
-   bool operator() (Record* x, Record* y) {
-     ComparisonEngine ceng;
-     return ceng.Compare(x,y,this->order) < 0 ? true : false;
-   }
-};
-
 class BigQ {
 
+	File tempFile;
+	Record myRecord;
+	Page myPage;
+	Page sortPage;
+	int pageCounter;
+	int runCounter;
+	vector<Record *> myVector;
+	vector<int> runL;
+
 public:
 
+	char tempFileName[10];
+	Pipe *in;
+	Pipe *out;
+	OrderMaker orderMaker;
+	int runLength;
+
 	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
+	BigQ();
 	~BigQ ();
+	void generateRuns(void);
+	void mergeRuns(void);
+	void sortAndWriteToFile(void);
+	void writeToFile(void);
+	void freeResourcesPhaseOne(void);
+};
+
+class CompareRecords{
+    ComparisonEngine compEngine;
+	OrderMaker* sortOrder;
+public:
+	CompareRecords(OrderMaker* sortorder)
+	{
+		sortOrder=sortorder;
+	}
+	bool operator()(Record *R1,Record *R2)
+	{
+		return compEngine.Compare(R1,R2,sortOrder)<0;
+	}
+};
+
+class RunRecord{
+
+public:
+	Record record;
+	int runNum;
+};
+
+class CompareQueues{
+    ComparisonEngine compEngine;
+	OrderMaker* sortOrder;
+public:
+	CompareQueues(OrderMaker *sortorder)
+	{
+		sortOrder=sortorder;
+	}
+	bool operator()(RunRecord *runR1,RunRecord *runR2)
+	{
+	    return compEngine.Compare(&(runR1->record),&(runR2->record),sortOrder)<0;
+	}
 };
 
 #endif
