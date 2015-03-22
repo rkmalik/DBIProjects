fd5a4c2ef247fdca86ae9d1bcf73be06a6965719
rkmalik <rkmalik@ufl.edu>
2015-03-22 10:01:01 -0400

    Implemented the basic methods

--- src/RelOp.cc
+++ src/RelOp.cc
@@ -1,6 +1,11 @@
 #include "RelOp.h"
 
+
+/***************Implementation for Select File **************/
+
 void SelectFile::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+
 }
 
 void SelectFile::WaitUntilDone () {
@@ -8,5 +13,120 @@ void SelectFile::WaitUntilDone () {
 }
 
 void SelectFile::Use_n_Pages (int runlen) {
+
+}
+
+
+/***************Implementation for Select Pipe**************/
+
+void SelectPipe::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+
+}
+
+void SelectPipe::WaitUntilDone () {
+	// pthread_join (thread, NULL);
+}
+
+void SelectPipe::Use_n_Pages (int runlen) {
+
+}
+
+
+/***************Implementation for Project **************/
+
+void Project::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+
+}
+
+void Project::WaitUntilDone () {
+	// pthread_join (thread, NULL);
+}
+
+void Project::Use_n_Pages (int runlen) {
+
+}
+
+
+
+/***************Implementation for Join **************/
+
+void Join::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+
+}
+
+void Join::WaitUntilDone () {
+	// pthread_join (thread, NULL);
+}
+
+void Join::Use_n_Pages (int runlen) {
+
+}
+
+
+/***************Implementation for DuplicateRemoval **************/
+
+void DuplicateRemoval::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+
+}
+
+void DuplicateRemoval::WaitUntilDone () {
+	// pthread_join (thread, NULL);
+}
+
+void DuplicateRemoval::Use_n_Pages (int runlen) {
+
+}
+
+
+/***************Implementation for Sum **************/
+
+void Sum::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+
+}
+
+void Sum::WaitUntilDone () {
+	// pthread_join (thread, NULL);
+}
+
+void Sum::Use_n_Pages (int runlen) {
+
+}
+
+
+/***************Implementation for GroupBy**************/
+
+void GroupBy::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+
+}
+
+void GroupBy::WaitUntilDone () {
+	// pthread_join (thread, NULL);
+}
+
+void GroupBy::Use_n_Pages (int runlen) {
+
+}
+
+
+
+
+/***************Implementation for WriteOut**************/
+
+void WriteOut::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+
+}
+
+void WriteOut::WaitUntilDone () {
+	// pthread_join (thread, NULL);
+}
+
+void WriteOut::Use_n_Pages (int runlen) {
 
 }
